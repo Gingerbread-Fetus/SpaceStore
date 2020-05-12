3c643c071f80f07f48e14467326b775de7721ef9
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-05-11 20:18:46 -0600

    +Added toggling to the ability panel +Added the ability buttons being added to the panel TODO: Need wo work on hooking up the ability buttons to the actual abilities in some way.

--- SpaceRecettear/Assets/Scripts/Canvas Scripts/Haggling Scripts/HagglingCanvas.cs
+++ SpaceRecettear/Assets/Scripts/Canvas Scripts/Haggling Scripts/HagglingCanvas.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,14 +14,20 @@ public class HagglingCanvas : MonoBehaviour
     [SerializeField] TextMeshProUGUI itemValuetext;
     [SerializeField] TextMeshProUGUI playerGoldText;
     [SerializeField] TextMeshProUGUI offerText;
+    [SerializeField] GameObject abilityPanel;
+    [SerializeField] AbilityButton abilityButtonPrefab;
+
+    public GameObject AbilityPanel { get => abilityPanel; }
 
     void Start()
     {
         staminaSlider.maxValue = hagglingController.PlayerProfile.GetStamina();
         staminaSlider.value = staminaSlider.maxValue;
+        PopulateAbilityButtons();
+        abilityPanel.SetActive(false);
         gameObject.SetActive(false);
     }
-    
+
     public void RedrawAll()
     {
         desiredItemImage.sprite = hagglingController.DesiredItem.item.itemIcon;
@@ -28,5 +35,23 @@ public class HagglingCanvas : MonoBehaviour
         playerGoldText.text = hagglingController.PlayerInventory.GetCurrency().ToString();
         offerText.text = hagglingController.transaction.Offer.ToString();
         staminaSlider.value = hagglingController.CurrentStamina;
+    }
+
+    public void ToggleDealPanelActive()
+    {
+        if (abilityPanel.activeInHierarchy) { abilityPanel.SetActive(false); }
+        else { abilityPanel.SetActive(true); }
+    }
+
+    private void PopulateAbilityButtons()
+    {
+        List<Ability> abilityList = hagglingController.PlayerProfile.Abilities;
+        foreach(Ability ability in abilityList)
+        {
+            Debug.Log("Adding Ability: " + ability.aName);
+            var newButton = Instantiate(abilityButtonPrefab, abilityPanel.transform);
+            newButton.SetAbility(ability);
+            //TODO: hook up the new buttons on click event to the relevant ability
+        }
     }
 }
